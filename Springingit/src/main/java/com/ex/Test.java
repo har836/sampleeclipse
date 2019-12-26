867857854cd9ad29e092d813c25e637800443f4f
maram <maram@192.168.0.21>
2019-12-26 11:02:22 +0530

    2nd

--- Springingit/src/main/java/com/ex/Test.java
+++ Springingit/src/main/java/com/ex/Test.java
@@ -3,7 +3,9 @@ package com.ex;
 public class Test {
 
 	public static void main(String[] args) {
+		
 		System.out.println("success................");
+		System.out.println("To check the changes.......");
 	}
 
 }
